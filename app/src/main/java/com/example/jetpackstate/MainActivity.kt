bb570636eda4819e44280029518471d6e8e10290
dqq <lezniak.daniel@gmail.com>
2024-07-05 17:50:56 +0200

    step 5

--- app/src/main/java/com/example/jetpackstate/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstate/MainActivity.kt
@@ -11,6 +11,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,7 +37,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     private fun WatherCount(modifier: Modifier){
-        var count = 0
+        var count by rememberSaveable {
+            mutableIntStateOf(0)
+        }
         Column {
             Text(text = "You have $count", modifier = modifier.padding(16.dp))
             Button(onClick = { count++ }) {
