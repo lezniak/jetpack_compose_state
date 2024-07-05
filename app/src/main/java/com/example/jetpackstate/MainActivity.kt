2fc5551ba4dc8fa1e6181c2efa0c679739273d85
dqq <lezniak.daniel@gmail.com>
2024-07-05 17:48:54 +0200

    first 1-4 steps

--- app/src/main/java/com/example/jetpackstate/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstate/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpackstate.ui.theme.JetpackstateTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +23,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackstateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WatherCount(Modifier.padding(innerPadding))
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackstateTheme {
-        Greeting("Android")
+    @Composable
+    private fun WatherCount(modifier: Modifier){
+        var count = 0
+        Column {
+            Text(text = "You have $count", modifier = modifier.padding(16.dp))
+            Button(onClick = { count++ }) {
+                Text(text = "Increment")
+            }
+        }
+        
     }
 }
