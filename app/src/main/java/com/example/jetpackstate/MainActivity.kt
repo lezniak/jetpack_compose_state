eb4d9cace07a5e96d07df7e991354981dbc10264
dqq <lezniak.daniel@gmail.com>
2024-07-07 16:53:41 +0200

    step 11

--- app/src/main/java/com/example/jetpackstate/MainActivity.kt
+++ app/src/main/java/com/example/jetpackstate/MainActivity.kt
@@ -11,10 +11,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -26,9 +29,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetpackstate.model.WellnessTask
 import com.example.jetpackstate.ui.theme.JetpackstateTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,58 +44,107 @@ class MainActivity : ComponentActivity() {
         setContent {
             JetpackstateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CounterMenu(Modifier.padding(innerPadding))
+                    CounterScreen(Modifier.padding(innerPadding))
                 }
             }
         }
     }
 
     @Composable
-    private fun CounterMenu(modifier: Modifier){
+    private fun CounterScreen(modifier: Modifier) {
         var waterCounter by rememberSaveable {
             mutableStateOf(0)
         }
-        var juiceCounter by rememberSaveable {
-            mutableStateOf(1)
-        }
+        var list = remember  { getWellnessTasks().toMutableStateList() }
         Column(modifier) {
-            WaterCount(count = waterCounter, onIncrement = {waterCounter++}, onClear = {waterCounter=0})
-            WaterCount(count = juiceCounter, onIncrement = {juiceCounter*=2}, onClear = {juiceCounter=0})
+            WaterCount(
+                count = waterCounter,
+                onIncrement = { waterCounter++ },
+                onClear = { waterCounter = 0 })
+           WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task)} )
         }
     }
+
     @Composable
-    private fun WaterCount(modifier: Modifier = Modifier, count : Int, onIncrement : () -> Unit, onClear : () -> Unit){
+    private fun WaterCount(
+        modifier: Modifier = Modifier,
+        count: Int,
+        onIncrement: () -> Unit,
+        onClear: () -> Unit
+    ) {
 
         Column(modifier) {
             if (count > 0) {
-                    Text(text = "You have $count", modifier = modifier.padding(16.dp))
+                Text(text = "You have $count", modifier = modifier.padding(16.dp))
             }
-            Row(){
-                Button(onClick = { onIncrement()}, enabled = count < 10) {
+            Row() {
+                Button(onClick = { onIncrement() }, enabled = count < 10) {
                     Text(text = "Increment")
                 }
 
 
-                Button(onClick = { onClear() }, enabled = count >0 ) {
+                Button(onClick = { onClear() }, enabled = count > 0) {
                     Text(text = "Clear")
                 }
             }
         }
-        
+
+    }
+
+    fun getWellnessTasks() = List(30) { i -> WellnessTask(i, "Task # $i") }
+
+    @Composable
+    fun WellnessTasksList(
+        modifier: Modifier = Modifier,
+        list: List<WellnessTask>,
+        onCloseTask: (WellnessTask)-> Unit
+    ) {
+        LazyColumn(
+            modifier = modifier
+        ) {
+            items(items = list) { item ->
+                WellnessTaskItem(taskName = item.name, onClose = { onCloseTask(item)})
+            }
+        }
     }
 
     @Composable
-    private fun WellnessTaskItem(taskName : String, onClose : () -> Unit ,modifier: Modifier = Modifier){
-        Card {
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)) {
-                Text(text = taskName,modifier.weight(1f))
-                IconButton(onClick = onClose) {
-                    Icon(Icons.Filled.Close, contentDescription = null)
-                }
+    fun WellnessTaskItem(taskName: String,onClose: () -> Unit, modifier: Modifier = Modifier) {
+        var checkedState by rememberSaveable { mutableStateOf(false) }
+
+        WellnessTaskItem(
+            taskName = taskName,
+            checked = checkedState,
+            onCheckedChange = { newValue -> checkedState = newValue },
+            onClose = {onClose()},
+            modifier = modifier,
+        )
+    }
+
+    @Composable
+    fun WellnessTaskItem(
+        taskName: String,
+        checked: Boolean,
+        onCheckedChange: (Boolean) -> Unit,
+        onClose: () -> Unit,
+        modifier: Modifier = Modifier
+    ) {
+        Row(
+            modifier = modifier, verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 16.dp),
+                text = taskName
+            )
+            Checkbox(
+                checked = checked,
+                onCheckedChange = onCheckedChange
+            )
+            IconButton(onClick = onClose) {
+                Icon(Icons.Filled.Close, contentDescription = "Close")
             }
         }
-
     }
 }
---
+++ app/src/main/java/com/example/jetpackstate/model/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.example.jetpackstate.model
+
+data class WellnessTask(val id: Int, val name: String)
